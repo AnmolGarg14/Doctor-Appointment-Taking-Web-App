f6a485c2827aa805516c631ccacccc6b1b2daf3a
Anmol99535 <anmol.garg594@gmail.com>
2023-08-21 21:31:10 +0530

    1st Commit

---
+++ frontend/src/routes/Routers.jsx
@@ -0,0 +1,26 @@
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Services from "../pages/Services";
+import Signup from "../pages/Signup";
+import Doctors from "../pages/Doctors/Doctors";
+import DoctorDetails from "../pages/Doctors/DoctorDetails";
+
+import { Routes, Route } from "react-router-dom";
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/doctors" element={<Doctors />} />
+      <Route path="/doctors/:id" element={<DoctorDetails />} />
+    </Routes>
+  );
+};
+
+export default Routers;
