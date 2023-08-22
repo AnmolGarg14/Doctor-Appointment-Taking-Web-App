2ef999594137532e4f39d74dc8073fec596449c7
Anmol99535 <anmol.garg594@gmail.com>
2023-08-22 21:31:13 +0530

    Hero

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      {/* hero section */}
+      <>
+        <section className="hero_section pt-[60px] 2xl:h-[800px]">
+          <div className="container">
+            <div className="flex flex-col lg:flex-row gap-[90px] items-center justify-between">
+              {/* hero content */}
+              <div>
+                <div className="lg:w-[570px]">
+                  <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">
+                    We help patients live a healthy, longer life.
+                  </h1>
+                  <p className="text_para">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit
+                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum.
+                  </p>
+
+                  <button className="btn">Request an Appointment</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    </>
+  );
 };
 
 export default Home;
