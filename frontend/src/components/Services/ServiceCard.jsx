83d8fc9aacadd9ecdf1d664d0e4714304694146b
Anmol99535 <anmol.garg594@gmail.com>
2023-08-24 20:21:26 +0530

    Hero

---
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ServiceCard = ({ item, index }) => {
+  const { name, desc, bgColor, textColor } = item;
+
+  return <div className="py"></div>;
+};
+
+export default ServiceCard;
