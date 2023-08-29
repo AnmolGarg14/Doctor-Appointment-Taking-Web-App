575e592c39c1de28e07984c94513decdd7d3082f
Anmol99535 <anmol.garg594@gmail.com>
2023-08-29 15:04:07 +0530

    Frontend Complete

---
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { formateDate } from "../../utils/formateDate";
+
+const DoctorAbout = () => {
+  return (
+    <div>
+      <div>
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2">
+          About of
+          <span className="text-irisBlueColor font-bold text-[24px] leading-9">
+            Anmol Garg
+          </span>
+        </h3>
+        <p className="text_para">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed ullamco
+          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+          in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Education
+        </h3>
+
+        <ul className="pt-4 md:p-5">
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                {formateDate("05-15-2008")} - {formateDate("10-15-2012")}
+              </span>
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+
+          <li className="flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]">
+            <div>
+              <span className="text-irisBlueColor text-[15px] leading-6 font-semibold">
+                {formateDate("05-02-2006")} - {formateDate("04-20-2008")}
+              </span>
+              <p className="text-[16px] leading-6 font-medium text-textColor">
+                PHD in Surgeon
+              </p>
+            </div>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+        </ul>
+      </div>
+
+      <div className="mt-12">
+        <h3 className="text-[20px] leading-[30px] text-headingColor font-semibold">
+          Experience
+        </h3>
+
+        <ul className="grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formateDate("05-15-2008")} - {formateDate("10-15-2012")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+
+          <li className="p-4 rounded bg-[#fff9ea]">
+            <span className="text-yellowColor text-[15px] leading-6 font-semibold">
+              {formateDate("05-15-2008")} - {formateDate("10-15-2012")}
+            </span>
+            <p className="text-[16px] leading-6 font-medium text-textColor">
+              Sr. Surgeon
+            </p>
+            <p className="text-[14px] leading-5 font-medium text-textColor">
+              New Apollo Hospital, New York.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default DoctorAbout;
